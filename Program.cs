1391f9e35d3cb6734daac0d6f931b20429e78b40
Llewellyn Kruger <launch@krugertech.co.za>
2022-11-29 13:38:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using BlazorApp;
+using BlazorApp.ViewModels;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddTransient<TestViewModel>();
+
+builder.Services.AddMvvm();
+
+await builder.Build().RunAsync();
---
+++ ViewModels/TestViewModel.cs
@@ -0,0 +1,18 @@
+﻿using MvvmBlazor.ViewModel;
+using System.ComponentModel;
+
+namespace BlazorApp.ViewModels
+{
+    public class TestViewModel : ViewModelBase, IDisposable
+    {
+        public TestViewModel()
+        {
+
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
